f3958bccdbafa9af9f17888164d99a0385fe0876
Yogesh Bhatt <ygshbht@gmail.com>
2022-07-25 23:30:04 +0530

    Update design of newDapp page

--- src/pages/NewDapp.tsx
+++ src/pages/NewDapp.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import { init, useConnectWallet } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-import { ethers } from "ethers";
 import {
 	NETWORK_ID,
 	NETWORK_NAME,
@@ -9,7 +7,12 @@ import {
 } from "dapp/constants/constants";
 import Mint from "components/newDapp/Mint";
 import { Button } from "@mui/material";
-
+import React, { useEffect, useRef } from "react";
+import { Box, Stack, ThemeProvider } from "@mui/material";
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+import assets from "data/assets";
+import theme from "contexts/theme";
 // Sign up to get your free API key at https://explorer.blocknative.com/?signup=true
 const dappId = "1730eff0-9d50-4382-a3fe-89f0d34a2070";
 
@@ -31,31 +34,148 @@ init({
 	],
 });
 
-export default function App() {
+export default function NewApp() {
 	const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
 
-	// create an ethers provider
-	let ethersProvider;
+	const containerRef = useRef<HTMLDivElement>(null);
 
-	if (wallet) {
-		ethersProvider = new ethers.providers.Web3Provider(wallet.provider, "any");
-	}
+	useEffect(() => {
+		const container = containerRef.current as HTMLDivElement;
+		function handler() {
+			container.style.minHeight = window.innerHeight + "px";
+		}
+		handler();
+		window.addEventListener("resize", handler);
+		return () => {
+			window.removeEventListener("resize", handler);
+		};
+	}, []);
+	// position the element created by dapp library
+	useEffect(() => {
+		setInterval(() => {
+			if (wallet) {
+				let elem = document.querySelector("onboard-v2") as HTMLElement;
+				console.log({ elem });
+				if (!elem) return;
+				let parent = elem.shadowRoot as ShadowRoot;
+				console.log({ parent });
+
+				if (!parent) return;
+
+				let content = parent.querySelector("div");
+				console.log({ content });
+				if (!content) console.log("No-content");
+
+				if (!content) return;
+				content.style.top = "100px";
+			}
+		}, 1000);
+	}, [wallet]);
 
 	return (
 		<div>
-			<button
+			{/* <button
 				disabled={connecting}
 				onClick={() => (wallet ? disconnect(wallet) : connect())}
 			>
 				{connecting ? "connecting" : wallet ? "disconnect" : "connect"}
-			</button>
-			{wallet ? (
-				<Mint />
-			) : (
-				<Button onClick={() => (wallet ? disconnect(wallet) : connect())}>
-					Mint
-				</Button>
-			)}
+			</button> */}
+
+			<ThemeProvider
+				theme={{
+					...theme,
+					palette: { ...theme.palette, primary: theme.palette.success },
+				}}
+			>
+				<Stack
+					ref={containerRef}
+					sx={{
+						boxSizing: "border-box",
+						background:
+							"linear-gradient(0.13deg, #CCFFF0 0.1%, #FFFFFF 74.99%)",
+						minHeight: "100vh",
+						justifyContent: "space-between",
+					}}
+				>
+					<Box>
+						<Navbar
+							connectWalletBtn={
+								<Button
+									onClick={() => (wallet ? disconnect(wallet) : connect())}
+									startIcon={<img alt="" src={assets.wallet} />}
+									variant="contained"
+								>
+									{connecting
+										? "connecting"
+										: wallet
+										? "disconnect"
+										: "Connect Wallet"}{" "}
+								</Button>
+							}
+						/>
+						<img
+							style={{
+								width: "min(926px, 130%)",
+								position: "relative",
+								left: "50%",
+								transform: "translateX(-50%)",
+								display: "block",
+							}}
+							src={assets.mintPageImages}
+							alt="three fungus"
+						/>
+						{/* <img
+					style={{ width: "min(389px, 80%)", display: "block", margin: "auto" }}
+					src={assets.freeMintText}
+					alt="free mint"
+				/> */}
+						<Box
+							sx={{
+								// py: 10,
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+								flexDirection: "column",
+								gap: 10,
+								"&>*": {
+									minWwidth: 200,
+								},
+							}}
+						>
+							{/* <ConnectWallet /> */}
+							{wallet ? (
+								<Mint />
+							) : (
+								<Button
+									onClick={() => (wallet ? disconnect(wallet) : connect())}
+									sx={{ textTransform: "initial" }}
+									variant="contained"
+									size="large"
+									// color="success"
+									startIcon={<img alt="Mint icon" src={assets.medicalKit} />}
+								>
+									Mint a funga
+								</Button>
+							)}
+						</Box>
+					</Box>
+					<Footer
+						sx={{
+							color: "black",
+							".all-rights-reserverd": {
+								opacity: "1 !important",
+								color: "black",
+							},
+							".connect-wallet-button": {
+								display: "none !important",
+							},
+							".top-row, .links": {
+								flexDirection: "row !important",
+							},
+						}}
+					/>
+				</Stack>
+			</ThemeProvider>
 		</div>
 	);
 }
