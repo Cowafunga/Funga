59fd865e22d71775f187970be1d33cddba4c3a11
Yogesh Bhatt <ygshbht@gmail.com>
2022-07-27 01:06:25 +0530

    Styling changes for web3 library's UI

--- src/pages/NewDapp.tsx
+++ src/pages/NewDapp.tsx
@@ -121,6 +121,16 @@ export default function NewDapp() {
 				content.style.zIndex = "3";
 				content.style.background = "white";
 				content.style.width = "max-content";
+
+				const network = content.querySelector(".network") as HTMLDivElement;
+				const address = content.querySelector(".address") as HTMLDivElement;
+				const balance = content.querySelector(".balance") as HTMLDivElement;
+				console.log({ network, address, balance });
+				if (!network || !address || !balance) return;
+				balance.style.whiteSpace = "nowrap";
+				address.style.whiteSpace = "nowrap";
+
+				network.style.marginLeft = "10px";
 				// content.style.position = "absolute";
 				// content.style.left = "50%";
 				// content.style.transform = "translateX(-50%)";
